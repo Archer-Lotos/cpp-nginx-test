4bd5aec9c671cb4d17acd028848351af824305a0
Viacheslav <novikova835@gmail.com>
2023-08-28 12:19:59 +0300

    test

--- docker-isolation-provider.cpp
+++ docker-isolation-provider.cpp
@@ -1,7 +1,6 @@
-#include "nginx.h"
-#include "ngx_config.h"
-#include "ngx_core.h"
-#include "ngx_http.h"
+#include <ngx_config.h>
+#include <ngx_core.h>
+#include <ngx_http.h>
 
 
 static ngx_int_t ngx_http_module_init(ngx_conf_t *cf);
